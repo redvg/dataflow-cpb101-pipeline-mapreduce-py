1341331e086013c36f1d9811cac6c79662959561
redvg <redvg17@gmail.com>
2018-08-06 20:28:35 +0200

    improves runner

--- pkg_popularity_pipeline.py
+++ pkg_popularity_pipeline.py
@@ -4,41 +4,56 @@
 import apache_beam as beam
 import argparse
 
-def startsWith(line, term):
+def find_matching_lines(line, search_query):
+
    if line.startswith(term):
+
       yield line
 
-def splitPackageName(packageName):
-   """e.g. given com.example.appname.library.widgetname
-           returns com
-	           com.example
-                   com.example.appname
-      etc.
-   """
+def split_package_name(package_name):
+
    result = []
-   end = packageName.find('.')
+
+   end = package_name.find('.')
+
    while end > 0:
-      result.append(packageName[0:end])
-      end = packageName.find('.', end+1)
-   result.append(packageName)
+
+      result.append(package_name[0:end])
+
+      end = package_name.find('.', end + 1)
+
+   result.append(package_name)
+
    return result
 
-def getPackages(line, keyword):
+def resolve_packages(line, keyword):
+
    start = line.find(keyword) + len(keyword)
+
    end = line.find(';', start)
+
    if start < end:
-      packageName = line[start:end].strip()
-      return splitPackageName(packageName)
+
+      package_name = line[start:end].strip()
+
+      return split_package_name(package_name)
+
    return []
 
-def packageUse(line, keyword):
-   packages = getPackages(line, keyword)
+def resolve_package_usage(line, keyword):
+
+   packages = resolve_packages(line, keyword)
+
    for p in packages:
+
       yield (p, 1)
 
-def by_value(kv1, kv2):
+def compare_by_value(kv1, kv2):
+
    key1, value1 = kv1
+
    key2, value2 = kv2
+
    return value1 < value2
 
 if __name__ == '__main__':
@@ -56,10 +71,10 @@ if __name__ == '__main__':
    # find most used packages
    (p
       | 'GetJava' >> beam.io.ReadFromText(input)
-      | 'GetImports' >> beam.FlatMap(lambda line: startsWith(line, keyword))
-      | 'PackageUse' >> beam.FlatMap(lambda line: packageUse(line, keyword))
+      | 'GetImports' >> beam.FlatMap(lambda line: find_matching_lines(line, keyword))
+      | 'PackageUse' >> beam.FlatMap(lambda line: resolve_package_usage(line, keyword))
       | 'TotalUse' >> beam.CombinePerKey(sum)
-      | 'Top_5' >> beam.transforms.combiners.Top.Of(5, by_value)
+      | 'Top_5' >> beam.transforms.combiners.Top.Of(5, compare_by_value)
       | 'write' >> beam.io.WriteToText(output_prefix)
    )
 
